061b7cd026d8f3298f053085154d41315dd4c50f
Megan Cindric <megancindric@gmail.com>
2021-09-21 10:43:28 -0500

    Registering models, initial migration

--- django_pokemon_generator/attacks/models.py
+++ django_pokemon_generator/attacks/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Attacks(models.Model):
-    attack_name = models.Charfield(max_length = 20)
-    attack_power = models.IntegerField(max_length = 3)
+    attack_name = models.CharField(max_length = 20)
+    attack_power = models.IntegerField()
     attack_type = models.CharField(max_length = 20)
     attack_description = models.TextField(max_length = 200)
     status_condition = models.TextField(max_length = 10)
-    status_condition_chance = models.DecimalField(max = 1)
+    status_condition_chance = models.DecimalField(decimal_places = 2, max_digits = 3, default = 0)
 
 
 def __str__(self):
