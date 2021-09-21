77e3569a78140cf2613d8998bd876dab27c4268f
Megan Cindric <megancindric@gmail.com>
2021-09-21 10:51:48 -0500

    Changing status_condition from text to char field

--- django_pokemon_generator/attacks/models.py
+++ django_pokemon_generator/attacks/models.py
@@ -6,7 +6,7 @@ class Attack(models.Model):
     attack_power = models.IntegerField()
     attack_type = models.CharField(max_length = 20)
     attack_description = models.TextField(max_length = 200)
-    status_condition = models.TextField(max_length = 10)
+    status_condition = models.CharField(max_length = 10)
     status_condition_chance = models.DecimalField(decimal_places = 2, max_digits = 3, default = 0)
 
 
