c6e640173cc8cd05fe8534347ee804e154eba131
Megan Cindric <megancindric@gmail.com>
2021-09-21 10:27:12 -0500

    Init, creating pkmn & attack apps

---
+++ django_pokemon_generator/attacks/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Attacks(models.Model):
+    attack_name = models.Charfield(max_length = 20)
+    attack_power = models.IntegerField(max_length = 3)
+    attack_type = models.CharField(max_length = 20)
+    attack_description = models.TextField(max_length = 200)
+    status_condition = models.TextField(max_length = 10)
+    status_condition_chance = models.DecimalField(max = 1)
---
+++ django_pokemon_generator/pokemons/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Pokemon(models.Model):
+    name = models.Charfield(max_length = 20)
+    primary_type = models.Charfield(max_length = 20)
+    secondary_type = models.Charfield(max_length = 20)
+    region = models.Charfield(max_length = 20)
+    description = models.TextField(max_length = 200)
+    #TODO link attacks using attack_name, then can query DB to get stats when called
+    primary_attack = models.CharField(max_length = 20)
+    seconday_attack = models.CharField(max_length = 20)
